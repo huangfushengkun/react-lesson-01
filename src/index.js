5c71497dfb046be62f7a9fb025397aed71a13556
xzh.hf <xzh.hf@hangshutech.com>
2020-03-06 11:24:01 +0800

    classnames styled 的使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
-import ReactDom from "react-dom"
+import {render} from "react-dom"
+import classNames from "classnames"
+import styled from 'styled-components'
+import './index.css'
+class App extends React.Component {
+    render() {
+        const style = {color:'#F00'}
+        // styled-components 的使用
+        const Title = styled.h1`
+            color:#f90
+        `
+        return (
+            <div>
+                <Title>我的新疆之旅</Title>
+                <ol>
+                    <li style={style}>报名</li>
+                    <li className="has-text">体检</li>
+                    <li className={classNames('shen')}>使用classnames包动态添加className</li>
+                    <li className={classNames('shen',{'huangfu':true,'huiyan':false})}>使用classnames包动态添加className</li>
+                </ol>
+            </div>
+        )
+    }
+}
 
-
-// ReactDom.render(
-//     <h1>welocoeme React</h1>,
-//     document.querySelector('#root')
-// )
+render(
+    <App />,
+    document.querySelector('#root')
+)
